f1d03dcb50e5ffc91f874f90d782fdd2c6be087f
june lee <melodysdreamj@gmail.com>
2024-03-23 20:56:16 +0900

    Initial commit

---
+++ lib/util/_/initial_app/build_app_widget/build_my_app/device_preview_module/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverDevicePreviewModule(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
---
+++ lib/util/_/initial_app/ready_functions/after_material_app/device_preview_module/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyAfterMaterialApp()
+// Future<void> readyForDevicePreviewModule(BuildContext context) async {}

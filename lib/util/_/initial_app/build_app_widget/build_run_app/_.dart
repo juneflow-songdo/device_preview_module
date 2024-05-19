fc3483ab3fa78512a746d399b4e12a418ac5307f
june lee <melodysdreamj@gmail.com>
2024-05-19 17:05:11 +0900

    `

--- lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
+++ lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import '../../../../../main.dart';
 import '../../ready_functions/before_run_app/_.dart';
 import '../build_my_app/_.dart';
-
+import 'device_preview_module/_.dart';
 
 Future<void> buildApp() async {
   await readyBeforeRunApp();
   Widget childWidget = MyApp();
 
+  childWidget = coverDevicePreviewModule(childWidget);
+
+
   return runApp(childWidget);
 }
+
+
--- lib/util/_/initial_app/ready_functions/before_run_app/_.dart
+++ lib/util/_/initial_app/ready_functions/before_run_app/_.dart
@@ -3,9 +3,12 @@ import '../../../../../main.dart';
 import 'widgets_binding/_.dart';
 
 Future<void> readyBeforeRunApp() async {
-  if (_done) return; _done = true;
+  if (_done) return;
+  _done = true;
+  
   await readyForWidgetsBinding();
 
-}
-bool _done = false;
 
+}
+
+bool _done = false;
